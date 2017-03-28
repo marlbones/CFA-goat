8145bf8dfbdf58cc42f81390d04ed83db889e197
marlbones <mdimaanods@gmail.com>
2017-03-29 10:13:07 +1100

    added omniauth for facebook. Currently functioning. Need to add comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :posts
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
